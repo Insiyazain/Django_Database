065201c86344e413489dcc21dff7d594f2dea683
insiyazain <insiyazain123@gmail.com>
2024-11-18 12:27:29 +0500

    make template

---
+++ Book_outlet/Bookpage/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Book
+
+# Create your views here.
+
+def index(request):
+    books= Book.objects.all()
+    return render(request,"Bookpage/index.html",{
+       "books": books
+    })
