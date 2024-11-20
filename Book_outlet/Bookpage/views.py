ffae632cd1b16d8d8e4d6f1ef77dacfa6b92b1f2
insiyazain <insiyazain123@gmail.com>
2024-11-20 16:41:56 +0500

    How to make slug

--- Book_outlet/Bookpage/views.py
+++ Book_outlet/Bookpage/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Book
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -8,3 +11,18 @@ def index(request):
     return render(request,"Bookpage/index.html",{
        "books": books
     })
+
+def Book_detail(request, slug):
+ 
+  #try:
+    #book=Book.objects.get(pk=id)
+  #except:
+    #raise Http404("Book not found")  
+  book=get_object_or_404(Book, slug=slug)
+  return render(request, "Bookpage/book_detail.html",{
+        "title":book.title,
+        "author":book.author,
+        "rating":book.rating,
+        "is_bestseller":book.is_bestselling
+    }
+                  )
